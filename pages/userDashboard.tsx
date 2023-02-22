30b337f3dacfffca6dc9e0d1a8885983bf0f6429
tuanln1201 <tuanln.dev@gmail.com>
2023-02-22 16:34:53 +0700

    Update

--- pages/userDashboard.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import DashboardContent from "@/components/dashboard/DashboardContent";
-
-export default function adminDashboard() {
-  return <DashboardContent />;
-}
